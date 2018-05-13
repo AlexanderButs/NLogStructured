8c0f0aa8ccadfd83a88819681e94b0e5bea52bd3
Alexander Buts <alexandr.buts@gmail.com>
2018-05-13 09:36:59 -0700

    Trying NLog structured logging.

---
+++ NLogStructured/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using NLog;
+using NLog.StructuredLogging.Json;
+
+namespace HttpsWebRequest
+{
+    public class Program
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public static async Task Main(string[] args)
+        {
+            Logger.Info("Some message.");
+
+            try
+            {
+                throw new Exception("Test exception message.");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Exception is thrown.");
+            }
+
+            EventLogger.Info("Logon by {user} from {ip_address}", "Kenny", "127.0.0.1");
+
+            await Console.In.ReadLineAsync();
+        }
+
+        private static readonly ILogger EventLogger = LogManager.GetLogger("Events");
+    }
+}
